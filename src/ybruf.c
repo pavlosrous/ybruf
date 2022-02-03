4d06d26271b298e134741ce0b33ed34979d8d6d9
pavlosrous <prousoglou@suffolk.edu>
2022-02-03 04:00:39 -0500

    V1

--- src/ybruf.c
+++ src/ybruf.c
@@ -1,7 +1,7 @@
 #include <netinet/in.h>
 #include <signal.h>
 
-#include "ybruf.h"
+#include "/home/prousoglou/Desktop/ClonedRep/bravo-project/include/ybruf.h"
 
 // Global parameters
 const int MIN_SERVERS = 3, MAX_SERVERS = 10;
@@ -15,10 +15,47 @@ static bool done = false;
 static bool app_initialize(int argn, char *argv[])
 {
   /* 1 */
-  
+  if (argn > 2)
+  {
+    perror("More than 2 arguments\n");
+    return false;
+  }
+
+  else if (argn == 2)
+  {
+    int p2 = atoi(argv[1]); //coverting the second parameter into an integer
+    if (p2 < 0)
+    {
+      fprintf(stderr, "Usage: %s [%s]\n", argv[0],argv[1]);
+      return false;
+    }
+    else
+    {
+      PORT = p2;
+    }
+  }
+
   /* 2 */
-  
+  if (chdir(APP_WD) != 0) //returns -1 if it fails
+  {
+    perror("chdir() to APP_WD failed\n");
+    return false;
+  }
+
   /* 3 */
+  FILE *fptr;
+
+  fptr = fopen("ybruf.pid", "w");
+
+  if (fptr == NULL)
+  {
+    perror("File cannot be created or a case of a write error\n");
+    return false;
+  }
+  
+  int server_ID = getpid();
+  fprintf(fptr, "%d", server_ID);
+  fclose(fptr);
 
   /* 4 */
   return true;
@@ -30,54 +67,77 @@ static bool app_initialize(int argn, char *argv[])
  */
 static void app_terminate(int signo)
 {
+  if (signo==10){
+    done = true;
+  }
+  else{
+    exit(EXIT_SUCCESS);
+  }
+
+  
 #ifdef DEBUG
   fprintf(stderr, "Terminated by %d\n", signo);
 #endif
-  
 }
 
-// The main function 
+// The main function
 int main(int argn, char *argv[])
 {
-  // Inilialize the process
+
   bool status = app_initialize(argn, argv);
   if (status == false)
     return EXIT_FAILURE;
 
   // Create a socket
-  struct sockaddr_in serv_addr, cli_addr; 
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (-1 == sockfd) {
+  struct sockaddr_in serv_addr, cli_addr;
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0); // AF_INET since its internet socket, SOCK_STREAM to specify that we are using a TCP protocol and 0 also 
+  // characterizes the TCP socket
+  if (-1 == sockfd)
+  {
     perror("socket");
     return EXIT_FAILURE;
   }
 
   // Bind the socket and start listening on it
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
-  serv_addr.sin_port = htons(PORT);
-  if (   -1 == bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))
-      || -1 == listen(sockfd, 5 /* backlog */)) {
+  bzero((char *)&serv_addr, sizeof(serv_addr));//ask about this
+
+  serv_addr.sin_family = AF_INET; //sets the family of the address (internet address)
+
+  serv_addr.sin_addr.s_addr = INADDR_ANY; //server addres we will be connecting to. We use any address connected to our local machine
+
+  serv_addr.sin_port = htons(PORT); //we specify the port we are connecting to. We need to use the htons() to put the integer port in the right
+  // network byte order. htons() converts it into the right data format so that our struct understands the port number
+
+  if (-1 == bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) || -1 == listen(sockfd, 5 /* backlog */))
+  {//if we wanted to connected to a socket in another machine, we would use connect() instead. Here we create a server (not a client). We bind it to a 
+  //socket number and IP. Listen function listens for connections
+  //bind specifies from where our sockets listens for connections
+  //if 0, everything is ok, if -1 something went wrong and return error message
     perror("bind/listen");
     return EXIT_FAILURE;
   }
-  
+
   signal(SIGUSR1, app_terminate);
   signal(SIGUSR2, app_terminate);
 
   // The main loop
-  while (!done) {
+  while (!done)
+  {
     unsigned clilen = sizeof(cli_addr);
     int newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-    if (-1 == newsockfd) { // What can we do? Cannot even print an error message
-      #ifdef DEBUG
+    //when we listen to a connection, we accept it and we get back a socket to manipulate anything on the client side so we have a two way connection 
+    //with the client and server
+    if (-1 == newsockfd)
+    { // What can we do? Cannot even print an error message
+#ifdef DEBUG
       perror("accept");
-      #endif
+#endif
       continue;
     }
     fputs("Hello, world!\n", stderr);
   }
+  close(sockfd);
   return EXIT_SUCCESS;
+
 }
 
