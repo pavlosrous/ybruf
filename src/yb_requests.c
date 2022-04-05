1854310159ffd7cd4bd6a7a805667ef7c96c075f
pavlosrous <prousoglou@suffolk.edu>
2022-04-05 19:49:48 -0300

    hw5-final

--- src/yb_requests.c
+++ src/yb_requests.c
@@ -3,7 +3,7 @@
 #include <dirent.h>
 #include <limits.h>
 
-#include "ybruf.h"
+#include "/home/prousoglou/Desktop/Team-bravo/bravo-project/include/ybruf.h"
 
 // Memory configuration parameters
 static const int MAX_RQ_SIZE = 1048576; // 1MB
@@ -26,25 +26,69 @@ static bool write_http_header(int sock_id,
 
 static bool process_dir(int sock_id, char *doc)
 {
-  // Open the directory for reading
-  // YOUR CODE GOES HERE
 
-  // Write the header - we seem to be ok
-  // Technically, this is not correct. We must first fully read the directory 
-  // YOUR CODE GOES HERE
+  struct dirent *d_info; //info about directory entry
 
+  // // Open the directory for reading
+  // YOUR CODE GOES HERE
+  DIR *dir = opendir(doc); 
+
+
+  //error checking for opening dir
+  if (dir == NULL){
+    syslog(LOG_ERR, "opendir(): %s", strerror(errno));
+    return false;
+  }
+  
   // Define the template
-  // YOUR CODE GOES HERE
+  char *ul_open = "<ul>\n";
+  char *ul_close = "</ul>";
+  char *li_open = "<li><a href=\"";
+  char *a_close = "\">";
+  char *li_close = "</a></li>\n";
+  char *html_start = "<html>\n<body>";
+  char *html_end = "</body>\n</html>";
+  char entry_name[1024];
+  
+  
+
+  write_http_header(sock_id, PROTO "200 OK", NULL);
+
+  write(sock_id, html_start, strlen(html_start));
+  write(sock_id, ul_open, strlen(ul_open));
 
   errno = 0; // Assume no errors
-  // YOUR CODE GOES HERE
+
+  while ((d_info = readdir(dir)) != NULL){ //find files in dir
+
+    strcpy(entry_name, d_info->d_name); //add the name of the file
+
+    if (d_info->d_type==DT_DIR){ //with dont want current directory
+      strcat(entry_name, "/");
+    }
+
+    if (strcmp(d_info->d_name,".")!=0){
+
+      write(sock_id, li_open, strlen(li_open));
+      write(sock_id, entry_name, strlen(entry_name)); //write list tags in socket
+      write(sock_id, a_close, strlen(a_close));
+      write(sock_id, d_info->d_name, strlen(d_info->d_name));
+      write(sock_id, li_close, strlen(li_close));
+    }
     
+  }
+  write(sock_id, ul_close, strlen(ul_close)); //write the closing hmtl tags
+  write(sock_id, html_end, strlen(html_end));
+  closedir(dir);
+  
+  
   if (errno) {
-    // Too late to complain!
+    syslog(LOG_ERR, "%s",strerror(errno));
   }
   
   return errno == 0;
 }
+
 
 static bool process_GET(int sock_id, char *doc)
 {
